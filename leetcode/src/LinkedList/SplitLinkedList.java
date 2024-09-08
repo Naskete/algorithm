d1ffeb5fc51746ec3a40207a657fabb8ece9ac1d
naskete <1776795871@qq.com>
2024-09-08 12:53:52 +0800

    simple solution

--- leetcode/src/LinkedList/SplitLinkedList.java
+++ leetcode/src/LinkedList/SplitLinkedList.java
@@ -38,4 +38,28 @@ public class SplitLinkedList {
         }
         return res;
     }
+
+    public ListNode[] solution(ListNode head, int k) {
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        int quotient = n / k, remainder = n % k;
+
+        ListNode[] parts = new ListNode[k];
+        ListNode curr = head;
+        for (int i = 0; i < k && curr != null; i++) {
+            parts[i] = curr;
+            int partSize = quotient + (i < remainder ? 1 : 0);
+            for (int j = 1; j < partSize; j++) {
+                curr = curr.next;
+            }
+            ListNode next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+        return parts;
+    }
 }
