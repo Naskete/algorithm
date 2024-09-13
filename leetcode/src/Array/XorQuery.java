4e2444c0e3a84041c66ffe61bc5af25a54b1e996
naskete <1776795871@qq.com>
2024-09-13 23:03:07 +0800

    1310.xor queries of subarray

--- leetcode/src/Array/XorQuery.java
+++ leetcode/src/Array/XorQuery.java
@@ -3,19 +3,15 @@ package Array;
 public class XorQuery {
     public int[] xorQueries(int[] arr, int[][] queries) {
         int m = arr.length;
-        int[][] xor = new int[m][m];
-        for (int i = 0; i < m; i++) {
-            xor[i][i] = arr[i];
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < m; j++) {
-                xor[i][j] = xor[i][j - 1] ^ arr[j];
-            }
+        int[] xor = new int[m];
+        xor[0] = arr[0];
+        for (int i = 1; i < m; i++) {
+            xor[i] = arr[i] ^ xor[i - 1];
         }
         int n = queries.length;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
-            ans[i] = xor[queries[i][0]][queries[i][1]];
+            ans[i] = xor[queries[i][0]] ^ xor[queries[i][1]];
         }
         return ans;
     }
