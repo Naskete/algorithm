e3dfa1bb10e30b45535f536c43c028d9af924a1c
naskete <1776795871@qq.com>
2024-10-11 00:44:31 +0800

    solution(oom)

--- leetcode/src/Array/Ramp.java
+++ leetcode/src/Array/Ramp.java
@@ -8,15 +8,17 @@ public class Ramp {
         System.out.println(maximumWidthRamp(new int[]{9,8,1,0,1,9,4,0,4,1}));
     }
     public static int maximumWidthRamp(int[] nums) {
-        int ans = 0;
         int n = nums.length;
+        int[][] ramp = new int[n][n];
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            int j = n - 1;
-            while (j > i && nums[j] < nums[i]) {
-                j--;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] >= nums[i]) {
+                    ramp[i][j] = j - i;
+                }
+                max = Math.max(max, ramp[i][j]);
             }
-            ans = Math.max(ans, j - i);
         }
-        return ans;
+        return max;
     }
 }
