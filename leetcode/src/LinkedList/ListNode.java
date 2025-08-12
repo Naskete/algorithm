43df8cf1dc568068f04b54c8507b40894a1349ce
naskete <1776795871@qq.com>
2025-08-12 21:06:42 +0800

    feat:generate listnode from string

--- leetcode/src/LinkedList/ListNode.java
+++ leetcode/src/LinkedList/ListNode.java
@@ -24,5 +24,33 @@ public class ListNode {
             point = point.next;
         }
         System.out.print("null");
+        System.out.println();
+    }
+
+    /**
+     *
+     * @param num 数字
+     * @param reverse 顺序
+     * @return
+     * true 321 ===> 3 -> 2 -> 1 -> null
+     * false 321 ===> 1 -> 2 -> 3 -> null
+     */
+    public static ListNode generateListNode(String num, boolean reverse) {
+        if (num == null) {
+            return null;
+        }
+        String s = num;
+        if (reverse) {
+            StringBuilder builder = new StringBuilder(s);
+            s = builder.reverse().toString();
+        }
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - '0';
+            p.next = new ListNode(val);
+            p = p.next;
+        }
+        return head.next;
     }
 }
