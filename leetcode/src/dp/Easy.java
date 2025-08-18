cce5990d65c127532f7136bdc80111439be139bd
naskete <1776795871@qq.com>
2025-08-18 21:45:49 +0800

    the nth tribonacci number

--- leetcode/src/dp/Easy.java
+++ leetcode/src/dp/Easy.java
@@ -25,4 +25,22 @@ public class Easy {
         }
         return f[n];
     }
+
+    // https://leetcode.cn/problems/n-th-tribonacci-number/?envType=study-plan-v2&envId=dynamic-programming
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 1;
+        for (int i = 3; i < n + 1; i++) {
+            f[i] = f[i -1] + f[i - 2] + f[i - 3];
+        }
+        return f[n];
+    }
 }
