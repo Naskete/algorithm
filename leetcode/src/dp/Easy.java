812c4519af019b6670b35addc8d59b3b6db281dd
naskete <1776795871@qq.com>
2025-08-18 21:45:12 +0800

    fibonacci number

--- leetcode/src/dp/Easy.java
+++ leetcode/src/dp/Easy.java
@@ -11,4 +11,18 @@ public class Easy {
         }
         return dp[n];
     }
+
+    // https://leetcode.cn/problems/fibonacci-number/?envType=study-plan-v2&envId=dynamic-programming
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n+1; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
+    }
 }
