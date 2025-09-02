68747f88d291f13782d44803c56932d115ec2d51
naskete <1776795871@qq.com>
2025-09-02 20:44:12 +0800

    preOrder travel

--- leetcode/src/Tree/TreeNode.java
+++ leetcode/src/Tree/TreeNode.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import java.util.Stack;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -16,5 +18,37 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static void preOrderTravel(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        preOrderTravel(root.left);
+        preOrderTravel(root.right);
+    }
+
+    // 先序遍历
+    // 1. 根入栈中
+    // 2. 打印（处理）弹出节点
+    // 3. 压入右孩子，再压入左孩子
+    // 4. 循环
+    public static void preOrderTravelUnRecur(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            System.out.println(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
     }
 }
