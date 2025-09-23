e42e47750a7f0013affebdccb72ba7e1a7d5862b
naskete <1776795871@qq.com>
2025-09-23 23:53:21 +0800

    count elements with max frequency

--- leetcode/src/daliy/Main.java
+++
@@ -1,8 +0,0 @@
-package daliy;
-
-public class Main {
-    public static void main(String[] args) {
-        Spreadsheet spreadsheet = new Spreadsheet(3);
-        System.out.println(spreadsheet.getValue("=3+5"));
-    }
-}
