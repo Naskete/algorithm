db30130d221c38022696efcf32abcc43406ad11b
naskete <1776795871@qq.com>
2025-10-27 17:07:21 +0800

    longest consecutive

---
+++ leetcode/src/Hot150/LongestConsecutive.java
@@ -0,0 +1,26 @@
+package Hot150;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer num :nums) {
+            set.add(num);
+        }
+        int ans = 0;
+        for (Integer num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int curLen = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    curLen++;
+                }
+                ans = Math.max(ans, curLen);
+            }
+        }
+        return ans;
+    }
+}
